271f31a88198889a7a29a10fdcd6033b25ca284c
BrayanASS <proffleandro.linux@gmail.com>
2024-07-23 11:03:52 -0300

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,9 @@ const skills = [
   {
     id: 0,
     name: "Graphic/UI Design",
+    nome: "Design Gráfico/UI",
     description: `solve visual problems by simplifying people&apos;s lives by providing satisfying experiences;`,
+    descricao: `resolver problemas visuais ao simplificar a vida das pessoas através da oferta de experiências satisfatórias;`,
     tools: [
       {
         id: 0.0,
@@ -36,8 +38,11 @@ const skills = [
   {
     id: 1,
     name: "Front-end",
+    nome: "Front-end",
     description:
       "development of the graphical interface where the user can view and interact with this website;",
+    descricao:
+      "desenvolvimento da interface gráfica onde o usuário pode visualizar e interagir com este site;",
     tools: [
       {
         id: 1.0,
@@ -64,8 +69,11 @@ const skills = [
   {
     id: 2,
     name: "Tools",
+    nome: "Ferramentas",
     description:
       "tools used to facilitate or speed up some of our services or perform specific actions;",
+    descricao:
+      "ferramentas utilizadas para facilitar ou acelerar alguns de nossos serviços ou realizar ações específicas;",
     tools: [
       {
         id: 2.0,
@@ -95,10 +103,12 @@ const projects = [
   {
     id: 0,
     image: "/images/tutty.png",
-    link: "https://github.com/BrayanAmarals/tuttymotors",
+    link: "https://bas-tuttymotors-v2.vercel.app/",
     name: "Tutty Motors",
     description:
       "Catalog website for the Tutty Motors automotive store. This site has an administrative login and product management system;",
+    descricao:
+      "Site de catálogo para a loja de automóveis Tutty Motors. Este site possui um sistema de login administrativo e gestão de produtos;",
     tags: ["NextJS", "Figma", "Firebase"],
   },
   {
@@ -108,6 +118,8 @@ const projects = [
     name: "Vortex Computadores",
     description:
       "Landing page developed for own development. Created from the beginning by me, this project was developed in NextJs, Tailwind and Shadcn, being the first time using this framework and library;",
+    descricao:
+      "Landing page desenvolvida para desenvolvimento próprio. Criado desde o início por mim, este projeto foi desenvolvido em NextJs, Tailwind e Shadcn, sendo a primeira vez que utilizo este framework e biblioteca;",
     tags: ["NextJS", "Shadcn", "Tailwind"],
   },
   {
@@ -117,6 +129,8 @@ const projects = [
     name: "Pet World",
     description:
       "Institutional website with catalog page, administrative login page and product management dashboard with removal, addition and editing functions. Powered by NextJS, Tailwind, and Firebase authentication and database services;",
+    descricao:
+      "Website institucional com página de catálogo, página de login administrativo e painel de gerenciamento de produtos com funções de remoção, adição e edição. Desenvolvido com NextJS, Tailwind e serviços de autenticação e banco de dados do Firebase;",
     tags: ["NextJS", "Tailwind", "Firebase"],
   },
   {
@@ -125,6 +139,8 @@ const projects = [
     link: "https://www.figma.com/design/47nVRt9rhKClqcfLNOBGLD/projeto-cqd?node-id=0-1&t=RvWcXu28V4nUdSpa-1",
     name: "Projeto CQD",
     description: `Class System Project, being the best planned design project I&apos;ve done to date, even though it&apos;s not completely executed, I believe it&apos;s interesting to bring it, because in it I was able to demonstrate a little that I learned after a while studying graphic design;`,
+    descricao:
+      "Projeto de Sistema de Aulas, sendo o projeto de design mais bem planejado que já fiz até agora, mesmo não estando completamente executado, acredito que seja interessante apresentá-lo, pois nele consegui demonstrar um pouco do que aprendi após algum tempo estudando design gráfico;",
     tags: ["Figma", "UI System"],
   },
 ];
@@ -134,6 +150,8 @@ export default function Home() {
   const aboutRef = useRef(null);
   const skillsRef = useRef(null);
   const projectsRef = useRef(null);
+  const [tanjiroPosition, setTanjiroPosition] = useState("up");
+  const [language, setLanguage] = useState("en");
 
   useEffect(() => {
     const handleScroll = () => {
@@ -144,17 +162,20 @@ export default function Home() {
         activeSection !== "about"
       ) {
         setActiveSection("about");
+        setTanjiroPosition("up");
       } else if (
         scrollPosition >= skillsRef.current.offsetTop &&
         scrollPosition < projectsRef.current.offsetTop &&
         activeSection !== "skills"
       ) {
         setActiveSection("skills");
+        setTanjiroPosition("mid");
       } else if (
         scrollPosition >= projectsRef.current.offsetTop &&
         activeSection !== "projects"
       ) {
         setActiveSection("projects");
+        setTanjiroPosition("down");
       }
     };
 
@@ -169,8 +190,29 @@ export default function Home() {
     setActiveSection(sectionId);
   };
 
+  const changeLanguage = () => {
+    if (language == "en") {
+      setLanguage("ptbr");
+    } else {
+      setLanguage("en");
+    }
+  };
+
   return (
     <main className="lg:flex lg:justify-between lg:gap-4 mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="flex flex-row gap-5 fixed top-5 right-10 items-center">
+        <p>{language == "en" ? "Language: " : "Idioma: "}</p>
+        <button onClick={() => changeLanguage()}>
+          <Icon
+            icon={
+              language == "en"
+                ? "emojione-v1:flag-for-united-states"
+                : "emojione-v1:flag-for-brazil"
+            }
+            className="text-4xl"
+          />
+        </button>
+      </div>
       <section className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
         <div>
           <div className="flex flex-col gap-3">
@@ -180,10 +222,14 @@ export default function Home() {
               </h1>
             </a>
             <h2 className="text-lg font-normal tracking-tight text-slate-200 sm:text-xl">
-              Junior Frontend Engineer
+              {language == "en"
+                ? "Junior Frontend Engineer"
+                : "Engenheiro Frontend Junior"}
             </h2>
             <h3 className="text-[#93A2B7] max-w-xs leading-regular mt-1">
-              I design and code beautifully simple things, and I love what I do.
+              {language == "en"
+                ? "I create works of art in design and programming, where simplicity is beautiful, and I do it with passion."
+                : "Eu crio obras de arte em design e programação, onde a simplicidade é bela, e faço isso com paixão."}
             </h3>
           </div>
           <div className="flex flex-col gap-6 mt-16 w-max">
@@ -205,7 +251,7 @@ export default function Home() {
                     : "group-hover:text-white group-focus-visible:text-white"
                 }`}
               >
-                ABOUT
+                {language == "en" ? "ABOUT" : "SOBRE"}
               </p>
             </a>
             <a
@@ -226,7 +272,7 @@ export default function Home() {
                     : "group-hover:text-white group-focus-visible:text-white"
                 }`}
               >
-                SKILLS
+                {language == "en" ? "SKILLS" : "HABILIDADES"}
               </p>
             </a>
             <a
@@ -247,11 +293,12 @@ export default function Home() {
                     : "group-hover:text-white group-focus-visible:text-white"
                 }`}
               >
-                PROJECTS
+                {language == "en" ? "PROJECTS" : "PROJETOS"}
               </p>
             </a>
           </div>
         </div>
+
         <ul className="ml-1 mt-8 flex items-center">
           <li className="mr-3 text-xs shrink-0">
             <Link
@@ -268,7 +315,8 @@ export default function Home() {
           <li className="mr-3 text-xs shrink-0">
             <Link
               href={"https://www.instagram.com/brayan_amarals"}
-              className="block "
+              className="block"
+              target="_blank"
             >
               <Icon
                 icon="mdi:instagram"
@@ -302,6 +350,14 @@ export default function Home() {
             </Link>
           </li>
         </ul>
+        <Image
+          src={`/images/tanjiro${tanjiroPosition}.png`}
+          alt="tanjiro image"
+          width={100}
+          height={100}
+          priority
+          className="absolute bottom-0 left-[50%]"
+        />
       </section>
       <section className="pt-24 lg:w-1/2 lg:py-24">
         <Image
@@ -315,24 +371,48 @@ export default function Home() {
         />
         <div className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-slate-400 mt-6">
           <h1 className="text-[#E2E8F0] text-3xl font-semibold tracking-tight">
-            About
+            {language == "en" ? "About" : "Sobre"}
           </h1>
           <p className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-slate-400 mt-6">
-            Hello! I a&apos;m Brayan Amaral, a
-            <b> Brazilian Web Developer from Goiânia-GO</b>, passionate about
-            front-end and UI/UX design, aiming to become an exceptional Full
-            Stack developer. <br />
-            <br /> At <b>22, with 4 years of programming experience</b>, I am
-            dedicated to professional growth. <br />
-            <br />I have a proven ability to develop customized and scalable
-            solutions, adhering to the best web development practices. My
-            excellent collaboration and communication skills enable me to work
-            effectively in team environments. I constantly keep up with the
-            latest industry trends and technologies to deliver high-quality,
-            impactful solutions to clients. <br />
-            <br />
-            <b>I am open to new challenges and opportunities </b>to contribute
-            to innovative web development projects.
+            {language == "en" ? (
+              <p>
+                Hello! I a&apos;m Brayan Amaral, a
+                <b> Brazilian Web Developer from Goiânia-GO</b>, passionate
+                about front-end and UI/UX design, aiming to become an
+                exceptional Full Stack developer. <br />
+                <br /> At <b>22, with 4 years of programming experience</b>, I
+                am dedicated to professional growth. <br />
+                <br />I have a proven ability to develop customized and scalable
+                solutions, adhering to the best web development practices. My
+                excellent collaboration and communication skills enable me to
+                work effectively in team environments. I constantly keep up with
+                the latest industry trends and technologies to deliver
+                high-quality, impactful solutions to clients. <br />
+                <br />
+                <b>I am open to new challenges and opportunities </b>to
+                contribute to innovative web development projects.
+              </p>
+            ) : (
+              <p>
+                Olá! Eu sou Brayan Amaral, um
+                <b> Desenvolvedor Web Brasileiro de Goiânia-GO</b>, apaixonado
+                por design front-end e UI/UX, com o objetivo de me tornar um
+                desenvolvedor Full Stack excepcional. <br />
+                <br /> Com <b>22 anos e 4 anos de experiência em programação</b>
+                , estou dedicado ao crescimento profissional. <br />
+                <br />
+                Tenho comprovada habilidade para desenvolver soluções
+                personalizadas e escaláveis, seguindo as melhores práticas de
+                desenvolvimento web. Minhas excelentes habilidades de
+                colaboração e comunicação me permitem trabalhar eficazmente em
+                ambientes de equipe. Estou sempre atualizado com as últimas
+                tendências e tecnologias da indústria para oferecer soluções de
+                alta qualidade e impacto aos clientes. <br />
+                <br />
+                <b>Estou aberto a novos desafios e oportunidades </b>para
+                contribuir em projetos inovadores de desenvolvimento web.
+              </p>
+            )}
           </p>
         </div>
         <div
@@ -340,7 +420,7 @@ export default function Home() {
           className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 text-slate-400 mt-6"
         >
           <h1 className="text-[#E2E8F0] text-3xl font-semibold tracking-tight">
-            Skills
+            {language == "en" ? "Skills" : "Habilidades"}
           </h1>
           {skills.map((skill) => {
             return (
@@ -350,10 +430,10 @@ export default function Home() {
               >
                 <div className="w-1/2 py-6">
                   <h1 className="text-[#5de7d2] text-xl font-medium tracking-tight mb-3">
-                    {skill.name}
+                    {language == "en" ? skill.name : skill.nome}
                   </h1>
                   <p className="text-slate-400 text-base font-regular tracking-tight">
-                    {skill.description}
+                    {language == "en" ? skill.description : skill.descricao}
                   </p>
                 </div>
                 <div className="grid grid-cols-2 gap-4">
@@ -381,7 +461,7 @@ export default function Home() {
           ref={projectsRef}
         >
           <h1 className="text-[#E2E8F0] text-3xl font-semibold tracking-tight mb-8">
-            Projects
+            {language == "en" ? "Projects" : "Projetos"}
           </h1>
 
           <div className="flex flex-col gap-6 group/list">
@@ -406,7 +486,9 @@ export default function Home() {
                         {project.name}
                       </h1>
                       <p className="text-slate-400 text-base font-regular tracking-tight">
-                        {project.description}
+                        {language == "en"
+                          ? project.description
+                          : project.descricao}
                       </p>
                       <div className="flex flex-row gap-2">
                         {project.tags.map((tag) => {
@@ -430,7 +512,10 @@ export default function Home() {
             href={"/projects"}
             className="text-[#E2E8F0] text-base font-medium tracking-tight flex flex-row gap-2 mt-10"
           >
-            View Full Portfolio <Icon icon="mdi:arrow-top-right"></Icon>
+            {language == "en"
+              ? "View Full Portfolio"
+              : "Ver Portfolio Completo"}{" "}
+            <Icon icon="mdi:arrow-top-right"></Icon>
           </Link>
         </div>
       </section>
